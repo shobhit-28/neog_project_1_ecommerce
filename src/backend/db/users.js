cc4c7f90dccb3330815390952488baba277886d0
shobhit-28 <shobhitraj34@gmail.com>
2023-05-16 21:56:39 +0530

    modified user

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "Shobhit",
+    lastName: "Raj",
+    email: "shobhitraj34.com",
+    password: "shobhit28",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
