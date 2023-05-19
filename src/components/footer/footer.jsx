9b936dc09bdfc719b9a56a6680d9dd7719ca78c5
shobhit-28 <shobhitraj34@gmail.com>
2023-05-19 21:44:55 +0530

    added footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,13 +3,16 @@ import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 
 export const Footer = () => {
-  return (
-    <div className="footer">
-        <div className="links">
-            <a href="https://twitter.com/Shobhit28573001" className="twitter"><FaTwitter /></a>
-            <a href="https://github.com/shobhit-28/" className="github"><FaGithub /></a>
-            <a href="https://www.linkedin.com/in/shobhit-raj-19a588215/" className="linkedin"><FaLinkedin /></a>
+    return (
+        <div className="footer">
+            <div className="links">
+                <a href="https://twitter.com/Shobhit28573001" className="twitter"><FaTwitter /></a>
+                <a href="https://github.com/shobhit-28/" className="github"><FaGithub /></a>
+                <a href="https://www.linkedin.com/in/shobhit-raj-19a588215/" className="linkedin"><FaLinkedin /></a>
+            </div>
+            <p className="copyright">
+                © No Copyright, Developed by <a href="https://github.com/shobhit-28/" className="shobhit">Shobhit Raj</a>
+            </p>
         </div>
-    </div>
-  )
+    )
 }
