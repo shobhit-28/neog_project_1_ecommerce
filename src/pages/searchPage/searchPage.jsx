0593a15420a876d08bcafe3e9d25a699768bb289
shobhit-28 <shobhitraj34@gmail.com>
2023-05-25 03:12:26 +0530

    styled search page

--- src/pages/searchPage/searchPage.jsx
+++ src/pages/searchPage/searchPage.jsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect } from 'react'
 import './searchPage.css'
 import { ProductReducerContext } from '../../contexts/productReducerContext/productReducerContext'
+import { Products } from '../../components/products/products'
 
 export const SearchPage = () => {
-    const { searchBarData, searchData, setIsSearchModalOpen } = useContext(ProductReducerContext)
+    const { searchBarData, searchData, setIsSearchModalOpen, setMenuState } = useContext(ProductReducerContext)
 
     const searchResults = searchData?.filter(({ title }) => title.slice(0, searchBarData?.length).toLowerCase() === searchBarData?.toLowerCase());
 
@@ -12,34 +13,40 @@ export const SearchPage = () => {
 
     useEffect(() => {
         setIsSearchModalOpen(false)
+        setMenuState(false)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
         <div className="search-page">
             {searchBarData.length === 0 ?
-                <p>Enter a keyword or product name in the search bar above to start exploring.</p>
+                <div className="data-not-found">
+                    <p>Enter a keyword or product name in the search bar above to start exploring.</p>
+                </div>
                 : searchData.length === 0 ?
-                    <p>Oops! We couldn't find any items matching your search criteria. Please try a different search term or browse through our categories to find what you're looking for.</p>
+                    <div className="data-not-found">
+                        <p>Oops! We couldn't find any items matching your search criteria. Please try a different search term or browse through our categories to find what you're looking for.</p>
+                    </div>
                     : <div className="data-found">
                         {searchResults?.length > 0 &&
                             <div className="data-search-results">
                                 <p className="heading">Search Results</p>
-                                {searchResults?.map((product) => (
-                                    <div className="product" key={product?._id}>
-                                        <p className="product-title">{product?.title}</p>
-                                    </div>
-                                ))}
+                                <div className="searched-products">
+                                    {searchResults?.map((product) => (
+                                        <Products product={product} key={product?.id} />
+                                    ))}
+                                </div>
+                                <hr />
                             </div>
                         }
                         {searchSuggestions?.length > 0 &&
                             <div className="data-search-suggestions">
                                 <p className="heading">Search Suggestions</p>
-                                {searchSuggestions?.map((product) => (
-                                    <div className="product" key={product?._id}>
-                                        <p className="product-title">{product?.title}</p>
-                                    </div>
-                                ))}
+                                <div className="search-suggestions">
+                                    {searchSuggestions?.map((product) => (
+                                        <Products product={product} key={product?.id} />
+                                    ))}
+                                </div>
                             </div>
                         }
                     </div>
