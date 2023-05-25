2d75a20323fb5932395805904742e5370e0d2f98
shobhit-28 <shobhitraj34@gmail.com>
2023-05-25 17:14:52 +0530

    styled login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -25,20 +25,27 @@ export const Login = () => {
     return (
         <div className="login-page">
             <div className="login-modal">
+                <p className="login-heading">
+                    Login
+                </p>
                 <form action="" className="login">
-                    <label className="email">
-                        <span className="input-title">Email: </span>
-                        <input type="email" name="" id="email" onChange={emailChangeHandler} />
-                    </label>
-                    <label className="password">
-                        <span className="input-title">Password: </span>
-                        <input type="password" name="" id="password" onChange={passwordChangeHandler} />
-                    </label>
+                    <div className="input-field-container">
+                        <label className="email">
+                            <span className="input-title">Email: </span>
+                            <input type="email" name="" id="email" onChange={emailChangeHandler} />
+                        </label>
+                    </div>
+                    <div className="input-field-container">
+                        <label className="password">
+                            <span className="input-title">Password: </span>
+                            <input type="password" name="" id="password" onChange={passwordChangeHandler} />
+                        </label>
+                    </div>
                 </form>
                 <div className='buttons'>
-                    <button className="login" onClick={() => login(loginInputData)} >Login</button>
-                    <button className="test-login" onClick={testLogin}>Test Login</button>
-                    <button className="signup" onClick={signUpClickHandler}>Sign Up</button>
+                    <button className="login login-btn" onClick={() => login(loginInputData)} >Login</button>
+                    <button className="test-login login-btn" onClick={testLogin}>Test Login</button>
+                    <button className="signup" onClick={signUpClickHandler}>{`Create an Account >`}</button>
                 </div>
             </div>
         </div>
